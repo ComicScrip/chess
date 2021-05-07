4237861d8f1102c465a3b7d30c61a6ab83090097
Pierre Genthon <pierre.genthon@wildcodeschool.com>
2021-05-07 11:31:18 +0200

    init

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import Chessboard from '../components/Chessboard'
 
 function Home() {
-  return <div className="h-screen p-12 bg-yellow-100 font-sans tracking-wider">hello</div>
+  return (
+    <div className="h-screen p-12 bg-yellow-100 font-sans tracking-wider">
+      <Chessboard />
+    </div>
+  )
 }
 
 export default Home
