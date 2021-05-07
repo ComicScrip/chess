cf05b9597162c37e83a164484c6c8b3b98b5dfac
Pierre Genthon <pierre.genthon@wildcodeschool.com>
2021-05-07 11:18:02 +0200

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
 import React from 'react'
-import Diagram from "./pages/Diagram";
-import {BrowserRouter, Switch, Route} from "react-router-dom";
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import Home from './pages/Home'
 
 const App = () => {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path='/'>
-          <Diagram/>
-        </Route>
+        <Route exact path="/" component={Home} />
       </Switch>
     </BrowserRouter>
-  );
-};
+  )
+}
 
-export default App;
+export default App
---
+++ src/pages/Home.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function Home() {
+  return <div className="h-screen p-12 bg-yellow-100 font-sans tracking-wider">hello</div>
+}
+
+export default Home
